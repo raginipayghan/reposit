fa7c0d5c956fbbd1727367120462730c51753816
crave <crave@vilas-PC>
2015-11-06 15:02:39 +0530

    new hospital project

---
+++ newtile/WebContent/newtile/tile.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("newtile.tile", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf newtile.tile
+	*/ 
+	getControllerName : function() {
+		return "newtile.tile";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf newtile.tile
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
